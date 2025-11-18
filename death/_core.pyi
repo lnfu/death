d83fd301e166965a1df30d374ff0c629009ff00d
Enfu Liao <efliao@cs.nycu.edu.tw>
2025-11-18 16:10:48 +0800

    chore: add ci (ruff & pytest)

--- death/_core.pyi
+++ death/_core.pyi
@@ -1,54 +1,53 @@
 """
 IRLS Solver core module
 """
+
 from __future__ import annotations
 import numpy
 import numpy.typing
 import typing
-__all__: list[str] = ['IRLSOptions', 'IRLSResult', 'IRLSSolver']
+
+__all__: list[str] = ["IRLSOptions", "IRLSResult", "IRLSSolver"]
+
 class IRLSOptions:
-    def __init__(self) -> None:
-        ...
+    def __init__(self) -> None: ...
     @property
-    def max_iter(self) -> int:
-        ...
+    def max_iter(self) -> int: ...
     @max_iter.setter
-    def max_iter(self, arg0: typing.SupportsInt) -> None:
-        ...
+    def max_iter(self, arg0: typing.SupportsInt) -> None: ...
     @property
-    def tol(self) -> float:
-        ...
+    def tol(self) -> float: ...
     @tol.setter
-    def tol(self, arg0: typing.SupportsFloat) -> None:
-        ...
+    def tol(self, arg0: typing.SupportsFloat) -> None: ...
+
 class IRLSResult:
     @property
-    def coefficients(self) -> list[float]:
-        ...
+    def coefficients(self) -> list[float]: ...
     @property
-    def converged(self) -> bool:
-        ...
+    def converged(self) -> bool: ...
     @property
-    def deviance(self) -> float:
-        ...
+    def deviance(self) -> float: ...
     @property
-    def fitted_values(self) -> list[float]:
-        ...
+    def fitted_values(self) -> list[float]: ...
     @property
-    def iterations(self) -> int:
-        ...
+    def iterations(self) -> int: ...
     @property
-    def std_errors(self) -> list[float]:
-        ...
+    def std_errors(self) -> list[float]: ...
+
 class IRLSSolver:
     def __init__(self) -> None:
         """
         Initialize IRLS Solver
         """
-    def fit(self, X: numpy.typing.NDArray[numpy.float64], y: numpy.typing.NDArray[numpy.float64], opts: IRLSOptions = ...) -> IRLSResult:
+    def fit(
+        self,
+        X: numpy.typing.NDArray[numpy.float64],
+        y: numpy.typing.NDArray[numpy.float64],
+        opts: IRLSOptions = ...,
+    ) -> IRLSResult:
         """
         Fit an IRLS model.
-                         
+
         Parameters
         ----------
         X : numpy.ndarray, shape (n, p)
@@ -57,10 +56,11 @@ class IRLSSolver:
             Response vector
         opts : IRLSOptions, optional
             Solver options
-        
+
         Returns
         -------
         IRLSResult
             Fitting results
         """
-__version__: str = '0.1.0'
+
+__version__: str = "0.1.0"
